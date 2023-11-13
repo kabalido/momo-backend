c283d2b3945151c9cb1dc18b59b21257457482e3
Everaldo Menout <everaldo.ailton@gmail.com>
2023-11-13 12:50:53 +0000

    adding .gitignore file

--- get_invoices.php
+++ get_invoices.php
@@ -11,7 +11,7 @@ $db = DatabaseFactory::getDatabase(DatabaseType::ORACLE);
 //$query = "select A.ar_ref ref, A.ar_invdate month,  sum(B.ar_am_loc) total_payment  from ivm_invoice_record A inner join ivm_invoice_detail B on A.ar_ref =B.ar_ref where A.subno='$msisdn' and ar_status !=9 group by A.ar_ref, A.ar_invdate order by A.ar_ref";
 
 $query="select C.name, A.ar_ref ref, to_char(A.ar_invdate, 'DD/MM/YYYY') month,  ceil(sum(B.ar_am_loc)) total_payment  from ivm_invoice_record A inner join ivm_invoice_detail B on A.ar_ref =B.ar_ref 
-   inner join crm_user_info C on A.subno=C.subno where A.subno='$msisdn' and ar_status !=9 group by C.name, A.ar_ref, A.ar_invdate order by A.ar_ref";
+   inner join crm_user_info C on A.subno=C.subno where A.subno='$msisdn' and ar_status !=9 group by C.name, A.ar_ref, A.ar_invdate order by A.ar_ref desc";
 
 //sleep(3);
 
